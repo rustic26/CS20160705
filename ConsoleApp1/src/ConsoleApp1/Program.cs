82e08e380548758b7111ea093b117f51f7219758
rustic26 <rustic26@gmail.com>
2016-07-06 14:56:55 -0700

    Threading test

--- ConsoleApp1/src/ConsoleApp1/Program.cs
+++ ConsoleApp1/src/ConsoleApp1/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     public class Program
     {
+        public static int counter = 0;
+
         public static void Main(string[] args)
         {
+            ThreadPool.QueueUserWorkItem(Foo);
+            ThreadPool.QueueUserWorkItem(Foo);
+
             Console.WriteLine("Hello World.");
+            Console.ReadKey();
+        }
+
+        public static void Foo(Object bar)
+        {
+            Console.WriteLine(Interlocked.Increment(ref counter));
         }
     }
 }
