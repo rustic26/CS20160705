216b19ace3e3bf4d0ef8f5c90aff7d05bf369dd6
rustic26 <rustic26@gmail.com>
2016-07-06 22:16:55 -0700

    test proj ref

---
+++ ConsoleApp1/src/ClassLibrary1/ClassLib.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+
+    public class ClassLib
+    {
+        public static int counter = 0;
+
+        public static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(Foo);
+            ThreadPool.QueueUserWorkItem(Foo);
+
+            Console.WriteLine("Hello World.");
+            Console.ReadKey();
+        }
+
+        public static void Foo(Object bar)
+        {
+            Console.WriteLine(Interlocked.Increment(ref counter));
+        }
+    }
+}
